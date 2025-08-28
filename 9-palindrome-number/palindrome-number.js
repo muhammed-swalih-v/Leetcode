54790584b25710e41e4403cf8518b0656be28a61
muhammed-swalih-v <mshamil5048@gmail.com>
2025-08-28 20:06:44 +0530

    Time: 8 ms (47.61%) | Memory: 65.1 MB (26.57%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) return false; 
+
+    let str = x.toString();
+    let reversed = str.split("").reverse().join("");
+
+    return str === reversed;
+};
+
+console.log(isPalindrome(-121)); // false
+console.log(isPalindrome(121));  // true
